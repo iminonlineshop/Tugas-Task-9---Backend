333d2130fd1e2d76ac1bca016756125fe2df5bdc
Rohimin_TI07 <93963688+iminonlineshop@users.noreply.github.com>
2021-12-11 10:24:10 +0700

    Tugas Task Assignment 9 - Backend
    Nama : Rohimin
    Kelas : TI 07 Weekend 2020
    NIM : 0110220073
    Mata Kuliah : Pemrograman Backend

---
+++ FruitController.js
@@ -0,0 +1,32 @@
+const fruits = require("./fruits");
+
+
+function index() {
+    for (const fruit of fruits) {
+        console.log(fruit);
+    }
+}
+
+function store(name) {
+    fruits.push(name);
+    index();
+}
+
+function update(position, name) {
+    fruits[position] = name;
+    index();
+}
+
+
+function destroy(position) {
+    fruits.splice(position, 1);
+    index();
+}
+
+
+module.exports = {
+    index,
+    store,
+    update,
+    destroy
+};
---
+++ app.js
@@ -0,0 +1,19 @@
+const {
+  index,
+  store,
+  update,
+  destroy
+} = require("./FruitController.js");
+
+const main = () => {
+  console.log("Method index - Menampilkan Buah");
+  index();
+  console.log("\nMethod store - Menambahkan buah Pisang");
+  store("Pisang");
+  console.log("\nMethod update - Update data 0 menjadi Kelapa");
+  update(0, "Kelapa");
+  console.log("\nMethod destroy - Menghapus data 0");
+  destroy(0);
+};
+
+main();
